c3d7a487e5ded46feb10534c91269864cfff2d69
Dhruxy077 <138969934+Dhruxy077@users.noreply.github.com>
2025-05-12 12:45:22 +0530

    Time: 0 ms (100%), Space: 40.2 MB (80.01%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,6 +1,15 @@
 class Solution {
     public int fib(int n) {
-        if(n<=1) return n;
-        return fib(n-1)+fib(n-2);
+        // if(n<=1) return n;
+        // return fib(n-1)+fib(n-2);
+        if(n==0) return 0;
+        int a=0,b=1;
+        for(int i=1;i<n;i++){
+            int temp=a;
+            a=b;
+            b+=temp;
+        }
+
+        return b;
     }
 }
