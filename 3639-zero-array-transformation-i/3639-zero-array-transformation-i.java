89d174208111445a424ba65dbe06878c6d398e20
Dhruxy077 <138969934+Dhruxy077@users.noreply.github.com>
2025-05-20 11:11:01 +0530

    Time: 3 ms (79.52%), Space: 80.7 MB (91.11%) - LeetHub

---
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isZeroArray(int[] nums, int[][] queries) {
+        // int m=queries.length;
+        int n=nums.length;
+        int[] diff=new int[n+1];
+        
+        for(int[] query:queries){
+            diff[query[0]]++;
+            diff[query[1]+1]--;
+        }
+
+        int[] decrementCount=new int[n];
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=diff[i];
+            decrementCount[i]=sum;
+        }
+
+        for(int i=0;i<n;i++){
+            if(decrementCount[i]<nums[i]) return false;
+        }
+
+        return true;
+    }
+}
