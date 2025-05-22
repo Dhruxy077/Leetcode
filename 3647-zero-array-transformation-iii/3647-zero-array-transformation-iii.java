55b074760cc0c2198c2beb9f986e232f29d351e9
Dhruxy077 <138969934+Dhruxy077@users.noreply.github.com>
2025-05-22 23:09:39 +0530

    Time: 96 ms (6.33%), Space: 113.8 MB (7.6%) - LeetHub

--- 3647-zero-array-transformation-iii/3647-zero-array-transformation-iii.java
+++ 3647-zero-array-transformation-iii/3647-zero-array-transformation-iii.java
@@ -1,23 +1,33 @@
 class Solution {
     public int maxRemoval(int[] nums, int[][] queries) {
-        Arrays.sort(queries, (a, b) -> Integer.compare(a[0], b[0]));
-        PriorityQueue<Integer> available = new PriorityQueue<>(Collections.reverseOrder());
-        PriorityQueue<Integer> assigned = new PriorityQueue<>();
-        int count = 0;
-        for (int time = 0, k = 0; time < nums.length; time++) {
-            while (!assigned.isEmpty() && assigned.peek() < time)
-                assigned.poll();
-            while (k < queries.length && queries[k][0] <= time)
-                available.add(queries[k++][1]);
-            while (assigned.size() < nums[time]
-                   && !available.isEmpty()
-                   && available.peek() >= time) {
-                assigned.add(available.poll());
-                count++;
+        Arrays.sort(queries,(a,b)->Integer.compare(a[0],b[0]));
+        PriorityQueue<Integer> available=new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> usedQuery=new PriorityQueue<>();
+        int n=nums.length;
+        int m=queries.length;
+        int queryPos=0;
+        int appliedCount=0;
+        for(int i=0;i<n;i++){
+
+            while(queryPos<m && queries[queryPos][0]==i){
+                available.offer(queries[queryPos][1]);
+                queryPos++;
             }
-            if (assigned.size() < nums[time])
-                return -1;
+
+            nums[i]-=usedQuery.size();
+
+            while (nums[i] > 0 && !available.isEmpty() && available.peek() >= i) {
+                usedQuery.offer(available.poll());
+                nums[i]--;
+                appliedCount++;
+            }
+
+            if(nums[i]>0) return -1;
+
+            while(!usedQuery.isEmpty() && usedQuery.peek()==i){
+                usedQuery.poll();
+            }
         }
-        return queries.length - count;
+        return m-appliedCount;
     }
 }
